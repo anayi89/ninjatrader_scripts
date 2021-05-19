4f0b122e6c1da6d447c6a7e638099b8e455ccf3c
anayi89 <it.garry@gmail.com>
2021-05-19 12:15:32 -0400

    Add files via upload

---
+++ rsi_crossover.cs
@@ -0,0 +1,73 @@
+﻿#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.Indicators;
+using NinjaTrader.NinjaScript.DrawingTools;
+#endregion
+
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public class RSICrossOver : Strategy
+	{
+		protected override void OnStateChange()
+		{
+			if (State == State.SetDefaults)
+			{
+				Description									= @"Enter the description for your new custom Strategy here.";
+				Name										= "RSI Crossover";
+				Calculate									= Calculate.OnBarClose;
+				EntriesPerDirection							= 1;
+				EntryHandling								= EntryHandling.AllEntries;
+				IsExitOnSessionCloseStrategy				= true;
+				ExitOnSessionCloseSeconds					= 30;
+				IsFillLimitOnTouch							= false;
+				MaximumBarsLookBack							= MaximumBarsLookBack.TwoHundredFiftySix;
+				OrderFillResolution							= OrderFillResolution.Standard;
+				Slippage									= 0;
+				StartBehavior								= StartBehavior.WaitUntilFlat;
+				TimeInForce									= TimeInForce.Gtc;
+				TraceOrders									= false;
+				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
+				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
+				BarsRequiredToTrade							= 20;
+				IsInstantiatedOnEachOptimizationIteration	= true;
+			}
+			if (State == State.Configure)
+			{
+				AddDataSeries("ETHUSD", Data.BarsPeriodType.Minute, 1, Data.MarketDataType.Last);
+				// sell when there's a $100 profit
+				SetProfitTarget(CalculationMode.Currency, 100);
+				
+				// sell when there's a $50 loss
+				SetStopLoss(CalculationMode.Currency, 50);
+			}
+		}
+
+		protected override void OnBarUpdate()
+		{
+			if (BarsInProgress != 0) 
+				return;
+			
+			// buy when the 14-day RSI crosses above its average
+			if (CrossAbove(RSI(14, 3), RSI(14, 3).Avg, 1))
+				EnterLong();
+		}
+	}
+}
